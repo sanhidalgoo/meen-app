d6b6be46278375fd92f28953f0cc86467c3f0b71
Santiago Hidalgo-Ocampo <shidalgoo1@eafit.edu.co>
2022-02-16 11:34:57 -0500

    customized blogs

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const path = require('path');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/my_database', {useNewUrlParser: true});
+mongoose.connect('mongodb://localhost/my_database', { useNewUrlParser: true });
 
 const app = new express();
 
@@ -12,34 +12,39 @@ app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded());
 
+const BlogPost = require('./models/BlogPost');
+
 app.listen(4000, () => {
     console.log('App listening on port 4000');
 })
 
 
-app.get('/', (req, res) => {
-    res.render('index');
-})
+app.get('/', async (req, res) => {
+    const blogposts = await BlogPost.find({});
+    console.log(blogposts);
+    res.render('index', {
+        blogposts
+    });
+});
 
 app.get('/about', (req, res) => {
     res.render('about');
-})
+});
 
 app.get('/contact', (req, res) => {
     res.render('contact');
-})
+});
 
 app.get('/post', (req, res) => {
     res.render('contact');
-})
+});
 
 app.get('/posts/new', (req, res) => {
     res.render('create');
-})
+});
 
 
-app.post('/posts/store', (req, res) => {
-    console.log(req.body.title);
-    console.log(req.body.body);
+app.post('/posts/store', async (req, res) => {
+    await BlogPost.create(req.body);
     res.redirect('/');
 })
